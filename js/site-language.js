ed970162663d792f2e5dd1cd94089712e253fe8d
tsikaimr-wq <TsiKaiMr@gmail.com>
2026-04-07 21:30:45 +0800

    Defer mobile heavy data listeners

--- js/site-language.js
+++ js/site-language.js
@@ -207,7 +207,7 @@
       || host === "easycryptoguide.pages.dev";
   }
 
-  var MOBILE_BUILD_ID = "20260407e";
+  var MOBILE_BUILD_ID = "20260407f";
 
   function buildMobileEntryUrl(query, hash) {
     var params = new URLSearchParams(query || "");
